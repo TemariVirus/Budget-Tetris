d7611f445c73050a87b758ed0b9bfcba2081f9f5
Zemogus <kiyonnxperiaz@gmail.com>
2022-09-24 22:54:33 +0800

    Tweaked rating system

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace TetrisAI;
 
 using FastConsole;
+using System;
 using System.Windows.Input;
 using Tetris;
 
@@ -16,7 +17,7 @@ class Program
     const int MAX_LINES = 300;
     const int THINK_TIME_IN_MILLIS = 150, MOVE_DELAY_IN_MILLIS = 0;
     const int PLAY_TIMES = 3;
-    const double DELTA_TRESH = 0.77;
+    const double DELTA_TRESH = 0.1;
     static readonly string Population_path = AppDomain.CurrentDomain.BaseDirectory + @"Pops\rating.txt";
         
     static void Main()
@@ -69,6 +70,53 @@ class Program
         }
     }
 
+    static void FitnessFunctionAPL(NN[] networks, int gen, double compat_tresh)
+    {
+        int seed = new Random().Next();
+        foreach (NN network in networks)
+        {
+            if (network.Played) continue;
+
+            // Prepare games
+            Game game = new Game(5, seed);
+            Game.SetGames(new Game[] { game });
+
+            // Show info on console
+            FConsole.Set(FConsole.Width, FConsole.Height + 8);
+            FConsole.CursorVisible = false;
+            FConsole.WriteAt($" Gen: {gen}\n", 0, 28);
+            NN best_nn = networks.Aggregate((max, current) => (current.Played && current.Fitness > max.Fitness) ? current : max);
+            FConsole.WriteLine($" Best: {best_nn.Fitness} by {best_nn.Name}");
+            double avg = 0;
+            if (networks.Any(x => x.Played))
+                avg = networks.Where(x => x.Played).Average(x => x.Fitness);
+            FConsole.WriteLine($" Average Fitness: {avg}");
+            FConsole.WriteLine($" Average Size: {networks.Average(x => x.GetSize())}");
+            FConsole.WriteLine($" No. of Species: {NN.Speciate(networks, compat_tresh).Count}");
+
+            // Start bots
+            Bot bot = new Bot(network, game);
+            game.Name = network.Name;
+            bot.Start(THINK_TIME_IN_MILLIS, MOVE_DELAY_IN_MILLIS);
+
+            // Wait bot dies or game takes too long
+            while (!game.IsDead && game.Lines < MAX_LINES)
+            {
+                game.WriteAt(0, 6, ConsoleColor.White, $"Sent: {game.Sent}".PadRight(11));
+                game.WriteAt(0, 22, ConsoleColor.White, $"APL: {Math.Round(game.APL, 3)}".PadRight(10));
+                Thread.Sleep((int)(1000 / FConsole.Framerate));
+            }
+
+            // End game
+            bot.Stop();
+            network.Fitness = game.Sent;
+            network.Played = true;
+
+            // Save progress
+            NN.SaveNNs(Population_path, networks, gen, compat_tresh);
+        }
+    }
+
     // Own rating system:
     //
     // Goals:
@@ -98,11 +146,11 @@ class Program
     //  -Having the lowest rating be as low as possible will allow for much higher ratings before hitting overflow.
     //   As ln(double.maxvalue) ~ 709.78, we will shift the ratings to make the highest one 700, then shift again to make the lowest one
     //   not smaller than -700 become -700 (ratings under -700 are considered negligible)
-    //  -Start with mean of 0, and sd of 100 (~99.9999999997% fall within 7 sd of the mean)
+    //  -Start with mean of 0, and sd of 3 (Chess ratings range from around 0-2800, which in our scale is roughly 0-16. With an sd of 8/3 ~3 we can expect that 99.7% of the ratings are within 3 sd)
     static void FitnessFunction(NN[] networks, int gen, double compat_tresh)
     {
         const double MinMu = -700, MaxMu = 700;
-        const double K = 1, A = 4, C = 3;
+        const double K = 1, A = 3, C = 4;
 
         // If it's the start of a new gen, reset mu and delta
         if (networks.All(x => !x.Played))
@@ -110,7 +158,7 @@ class Program
             foreach (NN network in networks)
             {
                 network.Mu = 0; // Maybe we can skip resetting this
-                network.Delta = 100;
+                network.Delta = 3;
             }
             networks[0].Played = true;
         }
@@ -119,7 +167,16 @@ class Program
         while (networks.Any(x => x.Delta > DELTA_TRESH))
         {
             double left_score = 0;
+            // First NN is the one with highest delta
             networks = networks.OrderByDescending(x => x.Delta).ToArray();
+            NN left = networks[0];
+            // Opponent is the one with the nearest rating that still needs to play
+            NN right = networks.Where(x => x.Delta > DELTA_TRESH && x != left)
+                               .Aggregate((networks[1], double.PositiveInfinity),
+                                          (tuple, current) => (Math.Abs(current.Mu - left.Mu) < tuple.Item2) ?
+                                                              (current, Math.Abs(current.Mu - left.Mu)) :
+                                                              tuple)
+                               .Item1;
 
             // Play 3(?) times
             for (int i = 0; i < PLAY_TIMES; i++)
@@ -140,13 +197,13 @@ class Program
                 FConsole.WriteAt($"No. of Species: {NN.Speciate(networks, compat_tresh).Count}", 1, 32);
 
                 // Start bots
-                Bot left = new Bot(networks[0], games[0]);
-                Bot right = new Bot(networks[1], games[1]);
-                left.Game.Name = networks[0].Name;
-                right.Game.Name = networks[1].Name;
+                Bot left_bot = new Bot(left, games[0]);
+                Bot right_bot = new Bot(right, games[1]);
+                left_bot.Game.Name = left.Name;
+                right_bot.Game.Name = right.Name;
                 Game.IsPaused = false;
-                left.Start(THINK_TIME_IN_MILLIS, MOVE_DELAY_IN_MILLIS);
-                right.Start(THINK_TIME_IN_MILLIS, MOVE_DELAY_IN_MILLIS);
+                left_bot.Start(THINK_TIME_IN_MILLIS, MOVE_DELAY_IN_MILLIS);
+                right_bot.Start(THINK_TIME_IN_MILLIS, MOVE_DELAY_IN_MILLIS);
 
                 // Wait until one dies or game takes too long
                 while (games.All(x => !x.IsDead) && games.All(x => x.Lines < MAX_LINES))
@@ -159,9 +216,9 @@ class Program
                     Thread.Sleep((int)(1000 / FConsole.Framerate));
                 }
                 // If only one is alive, wait a while to see if it kills itself
-                if (left.Game.IsDead ^ right.Game.IsDead)
+                if (left_bot.Game.IsDead ^ right_bot.Game.IsDead)
                 {
-                    Game alive = left.Game.IsDead ? right.Game : left.Game;
+                    Game alive = left_bot.Game.IsDead ? right_bot.Game : left_bot.Game;
                     int placed = alive.PiecesPlaced;
                     while (alive.PiecesPlaced - placed < 10 && !alive.IsDead)
                     {
@@ -176,13 +233,13 @@ class Program
 
                 // End game
                 Game.IsPaused = true;
-                left.Stop();
-                right.Stop();
+                left_bot.Stop();
+                right_bot.Stop();
 
-                if (left.Game.IsDead ^ right.Game.IsDead)
+                if (left_bot.Game.IsDead ^ right_bot.Game.IsDead)
                 {
                     // If left is only one surviving, left wins
-                    if (!left.Game.IsDead) left_score += 1D;
+                    if (!left_bot.Game.IsDead) left_score += 1D;
                 }
                 else
                 {
@@ -192,30 +249,35 @@ class Program
             }
 
             // Update rating mean and std dev
-            double P_left = 1 / (1 + Math.Exp(networks[1].Mu - networks[0].Mu));
+            double P_left = 1 / (1 + Math.Exp(right.Mu - left.Mu));
             left_score /= PLAY_TIMES;
             double error_left = left_score - P_left;
             double error_right = -error_left;
-            networks[0].Mu += K * networks[0].Delta * error_left;
-            networks[0].Delta *= 1 - Decay(Math.Abs(error_left));
-            networks[1].Mu += K * networks[1].Delta * error_right;
-            networks[1].Delta *= 1 - Decay(Math.Abs(error_right));
+            left.Mu += K * left.Delta * error_left;
+            left.Delta *= 1 - Decay(Math.Abs(error_left));
+            right.Mu += K * right.Delta * error_right;
+            right.Delta *= 1 - Decay(Math.Abs(error_right));
 
             // Save progress
             NN.SaveNNs(Population_path, networks, gen, compat_tresh);
         }
 
-        // Shift means so that the fittest is exactly the max
-        double shift = MaxMu - networks.Max(x => x.Mu);
-        foreach (NN network in networks) network.Mu += shift;
-        // Any means lower than the min are too small, so they are ignored
-        // Of the remaining means, we shift back down so that the least fit is excatly the min
-        shift = MinMu - networks.Min(x => (x.Mu < MinMu) ? double.PositiveInfinity : x.Mu);
-        foreach (NN network in networks) network.Mu += shift;
+        // If range of ratings is too big or too small, we'll have to shift them
+        if (networks.Min(x => x.Mu) < MinMu || networks.Max(x => x.Mu) > MaxMu)
+        {
+            // Shift means so that the fittest is exactly the max
+            double shift = MaxMu - networks.Max(x => x.Mu);
+            foreach (NN network in networks) network.Mu += shift;
+            // Any means lower than the min are too small, so they are ignored
+            // Of the remaining means, we shift back down so that the least fit is excatly the min
+            shift = MinMu - networks.Min(x => (x.Mu < MinMu) ? double.PositiveInfinity : x.Mu);
+            foreach (NN network in networks) network.Mu += shift;
+        }
         // Fitness = e^mean
         foreach (NN network in networks)
         {
-            network.Fitness = Math.Exp(network.Mu);
+            //network.Fitness = Math.Exp(network.Mu);
+            network.Fitness = Math.Pow(2, network.Mu);
             network.Played = true;
         }
         return;
