1654b0c84718b609524398e2b0042ed68697a952
Zemogus <kiyonnxperiaz@gmail.com>
2024-01-23 02:57:25 +0800

    Implement Tetra League replay player

--- dataset/build.zig
+++ dataset/build.zig
@@ -1,12 +1,30 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     const exe = b.addExecutable(.{
         .name = "dataset",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{}),
+        .root_source_file = .{ .path = "src/replay.zig" },
+        .target = target,
+        .optimize = optimize,
     });
+
+    // nterm dependency
+    const nterm_module = b.dependency("nterm", .{
+        .target = target,
+        .optimize = optimize,
+    }).module("nterm");
+    exe.root_module.addImport("nterm", nterm_module);
+
+    // Engine dependency
+    const engine_module = b.dependency("engine", .{
+        .target = target,
+        .optimize = optimize,
+    }).module("engine");
+    exe.root_module.addImport("engine", engine_module);
+
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
