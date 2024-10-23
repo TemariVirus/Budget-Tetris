c8d6fb53b5a90de12ea5a3230f41f9262504bbad
TEMARI VÍRUS <kiyonnxperiaz@gmail.com>
2024-10-24 00:07:39 +0800

    Remove sound features temporarily

--- build.zig
+++ build.zig
@@ -1,9 +1,5 @@
 const std = @import("std");
 const Build = std.Build;
-const builtin = std.builtin;
-
-const zwin32 = @import("zwin32");
-const zxaudio2 = @import("zxaudio2");
 
 pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
@@ -14,34 +10,19 @@ pub fn build(b: *Build) void {
         .target = target,
         .optimize = optimize,
     }).module("bot");
-    const zwin32_pkg = zwin32.package(b, target, optimize, .{});
-    const zxaudio2_pkg = zxaudio2.package(b, target, optimize, .{
-        .deps = .{ .zwin32 = zwin32_pkg.zwin32 },
-    });
 
-    _ = buildExe(
-        b,
-        target,
-        optimize,
-        bot_module,
-        zwin32_pkg.zwin32,
-        zxaudio2_pkg.zxaudio2,
-        zwin32_pkg.install_xaudio2,
-    );
+    _ = buildExe(b, target, optimize, bot_module);
 }
 
 fn buildExe(
     b: *Build,
     target: Build.ResolvedTarget,
-    optimize: builtin.OptimizeMode,
+    optimize: std.builtin.OptimizeMode,
     bot_module: *Build.Module,
-    zwin32_module: *Build.Module,
-    zxaudio2_module: *Build.Module,
-    install_xaudio2: *Build.Step,
 ) void {
     const exe = b.addExecutable(.{
         .name = "Budget Tetris",
-        .root_source_file = lazyPath(b, "src/main.zig"),
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -54,9 +35,6 @@ fn buildExe(
     exe.root_module.addImport("engine", engine_module);
     exe.root_module.addImport("bot", bot_module);
     exe.root_module.addImport("zmai", zmai_module);
-    exe.root_module.addImport("zwin32", zwin32_module);
-    exe.root_module.addImport("zxaudio2", zxaudio2_module);
-    exe.step.dependOn(install_xaudio2);
 
     if (exe.root_module.optimize == .ReleaseFast) {
         exe.root_module.strip = true;
@@ -64,7 +42,7 @@ fn buildExe(
 
     // Add NN files
     const install_NNs = b.addInstallDirectory(.{
-        .source_dir = lazyPath(b, "NNs"),
+        .source_dir = b.path("NNs"),
         .install_dir = .bin,
         .install_subdir = "NNs",
     });
@@ -72,7 +50,7 @@ fn buildExe(
 
     // Install sound assets
     const install_sounds = b.addInstallDirectory(.{
-        .source_dir = lazyPath(b, "assets/sound"),
+        .source_dir = b.path("assets/sound"),
         .install_dir = .bin,
         .install_subdir = "sound",
     });
@@ -88,13 +66,4 @@ fn buildExe(
     }
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-}
-
-fn lazyPath(b: *Build, path: []const u8) Build.LazyPath {
-    return .{
-        .src_path = .{
-            .owner = b,
-            .sub_path = path,
-        },
-    };
 }
